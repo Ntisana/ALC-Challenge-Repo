5e887bf1fff7a7d76f383940196875535a277d40
Ntisana <ntisanax@gmail.com>
2019-07-11 15:29:04 +0200

    First Commit of 5 Days of Code Challenge

---
+++ app/src/main/java/com/example/alc_phase_1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.alc_phase_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+* Name of Application:  ALC_Phase_1
+* Name of File:         MainActivity.java
+* Description:          This class will have two buttons that will link to each view consecutively
+* Date Created:         11 July 2019, 13:14
+* Created By/Author:    Xola Ntisana
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
