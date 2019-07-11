9426ad3feccd04e923cda3db7d49ef68662f05fd
Ntisana <ntisanax@gmail.com>
2019-07-11 15:43:44 +0200

    Query builder to the about page of Google ALC

---
+++ app/src/main/java/com/example/alc_phase_1/ApiUtil.java
@@ -0,0 +1,18 @@
+package com.example.alc_phase_1;
+
+/*
+ * Name of Application:  ALC_Phase_1
+ * Name of File:         ApiUtl.java
+ * Description:          This class will build queries to link to the About page of Google ALC
+ * Date Created:         11 July 2019, 15:31
+ * Created By/Author:    Xola Ntisana
+ * */
+
+import java.net.URL;
+
+public class ApiUtil {
+    private ApiUtil(){}                                                                                 // Removing the constructor as it will never be instantiated...
+
+    public static final String BASE_API_URL = "https://andela.com/alc/";
+
+}
